15e4acbf4184ddcede856b8c89c2e02e35dbb020
zoutao <1689613805@qq.com>
2019-06-13 17:24:57 +0800

    xuexi

---
+++ src/main/java/com/java8/java8study/lamda/MethodReference.java
@@ -0,0 +1,18 @@
+package com.java8.java8study.lamda;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * 功能描述:
+ *
+ * @author: MR.zt
+ * @date: 2019/5/25 23:25
+ */
+public class MethodReference {
+
+    public static void main(String[] args) {
+        List<String> list = Arrays.asList("hello", "zhangsan", "lisi");
+
+    }
+}
